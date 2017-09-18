65e0907e5d52101d2df3cfbf307cb3505bac9831
Oleg Larkin <krabique48@gmail.com>
2017-09-18 21:41:18 +0000

    Implement task's status change with a dropdown

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   load_and_authorize_resource :project
   load_and_authorize_resource :task, through: :project
+  before_action :task_status_options, only: [:new, :edit]
 
   def show
   end
@@ -21,8 +22,8 @@ class TasksController < ApplicationController
   end
 
   def update
-    if @task.update(update_task_params)
-      redirect_to @task, notice: 'Task was successfully updated.'
+    if @task.update(task_params)
+      redirect_to project_task_path(@project, @task), notice: 'Task was successfully updated.'
     else
       render :edit
     end
@@ -35,9 +36,17 @@ class TasksController < ApplicationController
 
 
   private
+
+  def task_status_options
+    @task_status_options = %w[waiting implementation verifying releasing]
+  end
   
   def task_params
-    params.require(:task).permit(:title, :description, :status, :user_id)
+    if task_status_options.include? params[:task][:status]
+      params.require(:task).permit(:title, :description, :status, :user_id)
+    else
+      params.require(:task).permit(:title, :description, :user_id)
+    end
   end
   
 end
