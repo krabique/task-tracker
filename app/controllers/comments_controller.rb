3fb600b90ad8d0582c879e191db82536125a35b2
Oleg Larkin <krabique48@gmail.com>
2017-09-19 18:09:21 +0000

    Add and implement show/create/update/delete comments in tasks

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,74 +1,58 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  # GET /comments
-  # GET /comments.json
+  load_and_authorize_resource :project
+  load_and_authorize_resource :task, through: :project
+  load_and_authorize_resource :comment, through: :task
+  
   def index
     @comments = Comment.all
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
   def show
   end
 
-  # GET /comments/new
   def new
-    @comment = Comment.new
   end
 
-  # GET /comments/1/edit
   def edit
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if safe_create_comment
+      redirect_to project_task_path(@project, @task), notice: 'Comment was successfully created.'
+    else
+      flash[:alert] = 'There was a problem creating the comment.'
+      render :new
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.update(comment_params)
+      redirect_to project_task_path(@project, @task), notice: 'Comment was successfully updated.'
+    else
+      flash[:alert] = 'There was a problem updating the comment.'
+      render :edit
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    Comment.find(params[:id]).destroy
+    redirect_to project_task_path(@project, @task), notice: 'Comment was successfully destroyed.'
   end
+
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:body, :task_id, :user_id)
-    end
+  
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+  
+  def safe_create_comment
+    begin
+      @comment = @task.comments.create! comment_params.merge(user: current_user)
+      return true
+    rescue ActiveRecord::RecordInvalid
+      return false
+    end     
+  end
+  
 end
