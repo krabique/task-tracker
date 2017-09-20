883514eb680edac3ec512d641681d78c245bd6d3
Oleg Larkin <krabique48@gmail.com>
2017-09-20 15:42:21 +0000

    Enhance db queries for projects list

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @projects = Project.all
+    @projects = Project.includes(:user).all
   end
 
   def show
