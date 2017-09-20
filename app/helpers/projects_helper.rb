a810d11fa3995ca03d6c575499ee282da2952f13
Oleg Larkin <krabique48@gmail.com>
2017-09-20 22:00:44 +0000

    Add color statuses to tasks lists in projects

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,5 @@
 module ProjectsHelper
+  def link_to_task_with_status_color(text, path, status, user_id)
+    link_to(text, path, class: "list-group-item #{'status-' + status}", "data-user-id": user_id)
+  end
 end
