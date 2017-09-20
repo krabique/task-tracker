f221d0bc53205d1d0127f2bf2a5a84b04043941d
Oleg Larkin <krabique48@gmail.com>
2017-09-20 20:19:59 +0000

    Add user's projects list to their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    if @user.manager_role?
+      @projects = Project.where(user: @user)
+    else
+      @projects = @user.projects
+    end
   end
   
 end
