1fe09429a98e47831c5a78778db51b632cb699fc
Oleg Larkin <krabique48@gmail.com>
2017-11-06 00:29:04 +0000

    Try sendgrid mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.secrets.mail_username,
+  :password => Rails.application.secrets.mail_password,
+  :domain => 'agile-cove-39191.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,25 +89,25 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  # -- For devise mailer -- start --
-  config.action_mailer.default_url_options = { host: 'agile-cove-39191.herokuapp.com', port: 80 }
+  # # -- For devise mailer -- start --
+  # config.action_mailer.default_url_options = { host: 'agile-cove-39191.herokuapp.com', port: 80 }
   
-  config.action_mailer.perform_caching = false 
+  # config.action_mailer.perform_caching = false 
   
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
   
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.smtp_settings = {
-    user_name:      Rails.application.secrets.mail_username,
-    password:       Rails.application.secrets.mail_password,
-    domain:         'gmail.com',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-  # -- For devise mailer -- end --
+  # config.action_mailer.smtp_settings = {
+  #   user_name:      Rails.application.secrets.mail_username,
+  #   password:       Rails.application.secrets.mail_password,
+  #   domain:         'gmail.com',
+  #   address:       'smtp.gmail.com',
+  #   port:          '587',
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
+  # # -- For devise mailer -- end --
 end
