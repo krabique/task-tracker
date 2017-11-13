4e5691939dcb19f9293030abd0a7f492a3a9c99a
Oleg Larkin <krabique48@gmail.com>
2017-11-13 12:54:36 +0000

    Delete obsolete comment from helper spec

--- spec/helpers/projects_helper_spec.rb
+++ spec/helpers/projects_helper_spec.rb
@@ -2,16 +2,6 @@
 
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ProjectsHelper. For example:
-#
-# describe ProjectsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ProjectsHelper, type: :helper do
   describe '.link_to_task_with_status_color(text, path, status, user_id)' do
     it 'should return a link to task with the according css class on it' do
