1bbf6e8d5262bd4acf7a507b69163220ec4ea502
Oleg Larkin <krabique48@gmail.com>
2017-11-13 14:20:28 +0000

    Delete obsolete files

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
 
