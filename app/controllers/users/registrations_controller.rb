86b8b01ee4b895b663374362131d061fd76af509
Oleg Larkin <krabique48@gmail.com>
2017-11-14 22:03:17 +0000

    Remove unused 'attribute' parameter from permitted user sign up parameters

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,8 +48,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: %i[attribute name manager_role developer_role])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: %i[name manager_role developer_role])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
